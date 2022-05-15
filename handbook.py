a9c4abd5bac3a7ae6c62e16137b752fb246f5594
Adam Chen <z5363476@ad.unsw.edu.au>
2022-05-15 17:40:36 +1000

    Attempt

--- handbook.py
+++ handbook.py
@@ -20,6 +20,12 @@ with open("./conditions.json") as f:
     CONDITIONS = json.load(f)
     f.close()
 
+def is_subset(a, b):
+    for item in a:
+        if not item in b:
+            return False
+    return True
+
 def is_unlocked(courses_list, target_course):
     """Given a list of course codes a student has taken, return true if the target_course 
     can be unlocked by them.
@@ -29,13 +35,45 @@ def is_unlocked(courses_list, target_course):
 
     You can assume all courses are worth 6 units of credit
     """
+    conditions_list = CONDITIONS[target_course].split()
     
-    # TODO: COMPLETE THIS FUNCTION!!!
-    
-    return True
+    if conditions_list == []:
+        return True
 
+    # handling uoc
+    if "units" in conditions_list and "credit" in conditions_list:
+        quota = int(conditions_list[conditions_list.index("units") - 1])
+        if len(courses_list) >= quota / 6:
+            return True
+        else:
+            return False
 
+    courses = []
+    or_cond = True
+    and_cond = False
+    open_brackets = 0
+    index = 0
+    courses_index = 0
+    while index < len(conditions_list):
+        item = conditions_list[index]
+        if item.lower() == 'or':
+            or_cond = True
+        elif item.lower() == 'and':
+            and_cond = True
+        elif item[0] == '(':
+            print(item)
+            open_brackets += 1
+        elif len(item) == 8:
+            if or_cond:
+                courses.append([item])
+                courses_index += 1
+                or_cond = False
+            elif and_cond:
+                courses[courses_index - 1].append(item)
+                and_cond = False
+        index += 1
 
-
-
-    
+    for condition in courses:
+        if is_subset(courses_list, condition):
+            return True
+    return False
